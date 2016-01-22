45768269af58321c82ef95a504dab290d97db0b4
Laurel Farris <laurel@laurelfarris.com>
2016-01-22 12:36:23 -0700

    homework updates

---
+++ homework/q3/README.md
@@ -0,0 +1,5 @@
+(1/25, 20 mins) What is the solar constant
+(definition and value, look it up if necessary)?
+What is the surface brightness of the Sun as seen from Earth?
+What would the solar constant and surface brightness of the Sun be
+   as seen from Jupiter?
---
+++ homework/q5/q5.py
@@ -0,0 +1,35 @@
+# Instructions: Determine the frequency and energy at
+# 5500 Angstroms, as well as the conversion factor between
+# Flux_lambda and Flux_frequency at 3000, 5500, and 8000 Angstroms.
+
+
+import numpy as np
+import math
+
+def WaveToFreq:
+    return 0
+
+def FreqToWave:
+    return 0
+
+def EnergyToWave:
+    return 0
+
+def WaveToEnergy:
+    return 0
+
+def EnergyToFreq:
+    return 0
+
+def FreqToEnergy:
+    return 0
+
+def Flux_FreqToWave:
+    return 0
+
+def Flux_WaveToFreq:
+    return 0
+
+wave1 = 3000.0
+wave2 = 5500.0
+wave3 = 8000.0
