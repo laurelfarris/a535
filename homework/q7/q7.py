5e36f5553c1bc6d1090e0ae3fab1120c8a2e38db
Laurel Farris <laurel@laurelfarris.com>
2016-02-01 07:17:56 -0700

    homework

--- homework/q7/q7.py
+++ homework/q7/q7.py
@@ -3,29 +3,33 @@ import pdb
 
 # Import 'conversions.py' from question 5
 import conversions
- 
-J = 10*1e-23 # Jansky [erg/s/cm^2/Hz]
-flux = 3.63e-20 # flux of Vega [erg/s/cm^2/Hz]
 
 # 1.
-
-#print '1. The flux of Vega is', flux/J, '[J].'
+J = 10*1e-23 # Jansky [erg/s/cm^2/Hz]
+wave = 5500.0e-8 # Wavelength [cm]
+flux_lambda = 3.6e-9 # flux of Vega [erg/s/cm^2/Angstrom]
+flux_nu = conversions.convert_flux(flux_lambda,wave)
+            # flux of Vega [erg/s/cm^2/Hz]
+print flux_nu
+print '1. The flux of Vega is', flux_nu/J, '[J].'
 
 # 2.
 
-flux_lambda = 3.60e-9
-wave = 8500.0e-8
-
-flux_nu = conversions.convert_flux(flux_lambda, wave)
-#print '2. The flux of the star is', flux_nu/J, '[J].'
+flux_lambda = 3.60e-9 # flux of star [erg/s/cm^2/Angstrom]
+wave = 8500.0e-8 # wavelength of star [cm]
+flux_nu = conversions.convert_flux(flux_lambda,wave)
+            # flux of star [erg/s/cm^2/Hz]
+print '2. The flux of the star is', flux_nu/J, '[J].'
 
 # 3.
 
-flux_star = 7.2e-14
-flux_Vega = 3.6e-9
+flux_star = 7.2e-14 # flux of star [erg/s/cm^2/Angstrom]
+flux_Vega = 3.6e-9 # flux of Vega [erg/s/cm^2/Angstrom]
 
-#print -2.5*math.log10(flux_star/flux_Vega)
-print flux_Vega/flux_star
+print '3. The difference in magnitudes is'
+print -2.5*math.log10(flux_star/flux_Vega)
+print 'and the star is', flux_Vega/flux_star
+print 'times fainter than Vega.'
 
 
 
