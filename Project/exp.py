5436e00e99cb7f1fd4fc061d1c13534463f7e0b2
laurelfarris <laurel07@nmsu.edu>
2016-04-04 10:31:45 -0600

    started etc

---
+++ Project/exp.py
@@ -0,0 +1,39 @@
+'''
+Atmospheric transmission, a (extinction, refraction, etc.)
+System effeciency, q
+Telescope throughput
+Instrument throughput
+Filter throughput
+Grating specification
+'''
+
+import math
+from astropy import constants as const
+
+''' Define constants '''
+c = const.c.cgs.value
+h = const.h.cgs.value
+
+def atm_transmission(wavelength, seeing, airmass, moon):
+    '''
+    Calculate the atmospheric transmission (a)
+    Haziness/transparency, seeing, airmass, etc.
+    '''
+    k = -1.086*tau
+    a = ...
+    return a
+
+def sys_transmission(wavelength):
+    '''
+    Calculate the system transmission (q), using the telescope
+    throughput, filter throughput, and detector efficiency; all three of
+    which are calculated within their own function embedded below. 
+    '''
+    def telescope_throughput():
+        return 0
+    def filter_throughput():
+        return 0
+    def detector_efficiency():
+        return 0
+    return q
+
